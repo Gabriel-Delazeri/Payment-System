aeebd1e13ed1e431ed97affe71d2ed705133827d
Gabriel-Delazeri <gbldelazeri@gmail.com>
2022-10-10 16:22:31 -0300

    feat: UserController Request Validations

---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name'        => 'required|min:3|max:30',
+            'document_id' => 'required|numeric' ,
+            'email'       => 'required|email',
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'min'      => 'O campo :attribute deve ter no mínimo :min caracteres',
+            'max'      => 'O campo :attribute deve ter no máximo :max caracteres',
+            'numeric'  => 'O campo :attribute deve ser preenchido somente por números',
+            'email'    => 'O campo :attribute deve receber um endereço de e-mail válido',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+        'errors' => $validator->errors(),
+        'status' => false
+        ], 422));
+    }
+}
